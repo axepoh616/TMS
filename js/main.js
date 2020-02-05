a618731c31641c7cff35ac86a2fee1f1e536f7b5
axepoh616 <57939572+axepoh616@users.noreply.github.com>
2020-02-05 23:59:34 +0300

    JSHW1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,37 @@
 
+// v
+// let NoOne = {
+//     name: "John",
+//     lastName: "Smith",
+//     age: "25",
+//     gender: "male"
+// };
+// let AbsolutelyNobody = {
+//     name: "Marta",
+//     lastName: "Miller",
+//     age: "23",
+//     gender: "female"
+// };
+// let obj.NotASingleSoulInGroup = {
+//     name: "Alex",
+//     lastName: "Pupochkin",
+//     age: "30",
+//     gender: "male"
+// };
+
+var NoOne = new Object();
+NoOne.name = "John";
+NoOne.lastName = "Smith";
+NoOne.age = "25";
+NoOne.gender = "male";
+var AbsolutelyNobody = new Object();
+AbsolutelyNobody.name = "John";
+AbsolutelyNobody.lastName = "Smith";
+AbsolutelyNobody.age = "25";
+AbsolutelyNobody.gender = "male";
+var NotASingleSoulInGroup = new Object();
+NotASingleSoulInGroup.name = "John";
+NotASingleSoulInGroup.lastName = "Smith";
+NotASingleSoulInGroup.age = "25";
+NotASingleSoulInGroup.gender = "male";
 let arr = ["NoOne", "AbsolutelyNobody", "NotASingleSoulInGroup"];
-let NoOne = {
-    name: "John",
-    lastName: "Smith",
-    age: "25",
-    gender: "male"
-};
-let AbsolutelyNobody = {
-    name: "Marta",
-    lastName: "Miller",
-    age: "23",
-    gender: "female"
-};
-let NotASingleSoulInGroup = {
-    name: "Alex",
-    lastName: "Pupochkin",
-    age: "30",
-    gender: "male"
-};
