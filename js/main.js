1762e71e600dac4d1e572b68078341d1bd2ae018
axepoh616 <57939572+axepoh616@users.noreply.github.com>
2020-02-05 23:27:22 +0300

    JSHW1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+let arr = ["NoOne", "AbsolutelyNobody", "NotASingleSoulInGroup"];
+let NoOne = {
+    name: "John",
+    lastName: "Smith",
+    age: "25",
+    gender: "male"
+};
+let AbsolutelyNobody = {
+    name: "Marta",
+    lastName: "Miller",
+    age: "23",
+    gender: "female"
+};
+let NotASingleSoulInGroup = {
+    name: "Alex",
+    lastName: "Pupochkin",
+    age: "30",
+    gender: "male"
+};
