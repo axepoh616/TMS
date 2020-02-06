60c213a2a83cffaae12d4c9de7d10d152e96c49a
axepoh616 <57939572+axepoh616@users.noreply.github.com>
2020-02-06 19:54:56 +0300

    JSHW1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 var obj1 = {
-    name: "John",
-    lastName: "Smith",
-    age: "25",
-    gender: "male"
+    name: John,
+    lastName: Smith,
+    age: 25,
+    male: true
 }
 var obj2 = {
-    name: "Marta",
-    lastName: "Miller",
-    age: "23",
-    gender: "female"
+    name: Marta,
+    lastName: Miller,
+    age: 23,
+    male: folse
 }
 var obj3 = {
-    name: "Alex",
-    lastName: "Pupochkin",
-    age: "30",
-    gender: "male"
+    name: Alex,
+    lastName: Pupochkin,
+    age: 30,
+    male: true
 }
 var arr = [obj1, obj2, obj3];
