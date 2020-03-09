a481bd78e782c9f2f19c56d95bc932606e2d7b3c
axepoh616 <57939572+axepoh616@users.noreply.github.com>
2020-03-10 00:51:26 +0300

    ok

--- calc/js/main.js
+++ calc/js/main.js
@@ -36,10 +36,11 @@ const submit = () => {
         const result = document.getElementById('result')
         const curId = select.options[select.selectedIndex].dataset.curId
         const rate = await http(`https://nbrb.by/api/exrates/rates/${curId}`)
+        const scale = await http(`https://nbrb.by/api/exrates/rates/${curId}`)
 
         const currentCurrencyValue = document.getElementById(`current_currency`).value
-        const composition = Number(currentCurrencyValue) / rate.Cur_OfficialRate//калькулируем значение методом деления заданного числа на курс полученный с сервака(не работает на рос рубли тк везде привязка к 1 а в рос рублях к 100!)
-        //тут гдето нужно ввести данные с сервака Cur_Scale=100 ? rate.Cur_OfficialRate * 100 для 1000 * 1000 и тд я хз какие могут быть варианты дольше ,но это не точно ! 
+        const composition = Number(currentCurrencyValue) / rate.Cur_OfficialRate * scale.Cur_Scale
+
         const str = `You Can by ${composition.toFixed(2)} ${rate.Cur_Abbreviation} for ${currentCurrencyValue} BYN -- ${getDate(rate.Date)}`
         result.innerHTML = str
     })
